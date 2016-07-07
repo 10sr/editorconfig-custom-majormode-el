4da944345cf900cada00b48788b6b55dedcaa084
10sr <8slashes+git@gmail.com>
2016-07-07 12:35:14 +0900

    Add example to Commentary section

--- editorconfig-custom-majormode.el
+++ editorconfig-custom-majormode.el
@@ -39,6 +39,12 @@
 ;; An EditorConfig extension that defines a property to specify which
 ;; Emacs major-mode to use for files.
 
+;; For example, add emacs_mode to your .editorconfig as follows, and
+;; `nginx-mode' will be always enabled when visiting *.conf files.
+
+;; [*.conf]
+;; emacs_mode = nginx
+
 ;; To enable this plugin, add `editorconfig-custom-majormode' to
 ;; `editorconfig-custom-hooks':
 
